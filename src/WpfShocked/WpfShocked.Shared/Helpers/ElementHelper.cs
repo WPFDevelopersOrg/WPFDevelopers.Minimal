cf15af75d589f96cd178e1bef6d4dde43dc77ab2
yanjinhuamail@gmail.com <yanjinhuamail@gmail.com>
2021-12-03 16:34:57 +0800

    add in WpfShocked.

---
+++ src/WpfShocked/WpfShocked.Shared/Helpers/ElementHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WpfShocked.Helpers
+{
+    public class ElementHelper : DependencyObject
+    {
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ElementHelper), new PropertyMetadata(new CornerRadius(3)));
+    }
