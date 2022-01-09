da42d63bc5b1e096eac1e34ce4725d33e583637d
yanjinhuagood <yanjinhua@gmail.com>
2022-01-09 20:00:02 +0800

    add in TabControl.

---
+++ src/WpfShocked/WpfShocked.Shared/Helpers/ElementConvert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfShocked.Helpers
+{
+    public class ElementConvert
+    {
+
+    }
+    public class WidthConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 20;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
