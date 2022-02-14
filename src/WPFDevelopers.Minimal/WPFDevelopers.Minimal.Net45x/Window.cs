131f0f5e4496616ef641468806e79070b7f77442
yanjinhuamail@gmail.com <yanjinhuamail@gmail.com>
2022-02-14 10:45:48 +0800

    update window.

--- src/WPFDevelopers.Minimal/WPFDevelopers.Minimal.Net45x/Window.cs
+++ src/WPFDevelopers.Minimal/WPFDevelopers.Minimal.Net45x/Window.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace WPFDevelopers.Minimal.Net45x
 {
@@ -31,13 +33,18 @@ namespace WPFDevelopers.Minimal.Net45x
         }
         public Window()
         {
+            this.Loaded += Window_Loaded;
             CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, CloseWindow));
             CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, MaximizeWindow, CanResizeWindow));
             CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, MinimizeWindow, CanMinimizeWindow));
             CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, RestoreWindow, CanResizeWindow));
             //CommandBindings.Add(new CommandBinding(SystemCommands.ShowSystemMenuCommand, ShowSystemMenu));
         }
-
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            hWnd = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(hWnd).AddHook(WindowProc);
+        }
         protected override void OnContentRendered(EventArgs e)
         {
             base.OnContentRendered(e);
@@ -70,14 +77,43 @@ namespace WPFDevelopers.Minimal.Net45x
 
         private void MinimizeWindow(object sender, ExecutedRoutedEventArgs e)
         {
-            SystemCommands.MinimizeWindow(this);
+            //SystemCommands.MinimizeWindow(this);
+            SendMessage(hWnd, ApiCodes.WM_SYSCOMMAND, new IntPtr(ApiCodes.SC_MINIMIZE), IntPtr.Zero);
         }
 
         private void RestoreWindow(object sender, ExecutedRoutedEventArgs e)
         {
             SystemCommands.RestoreWindow(this);
         }
-      
+        internal class ApiCodes
+        {
+            public const int SC_RESTORE = 0xF120;
+            public const int SC_MINIMIZE = 0xF020;
+            public const int WM_SYSCOMMAND = 0x0112;
+        }
+        private IntPtr hWnd;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == ApiCodes.WM_SYSCOMMAND)
+            {
+                if (wParam.ToInt32() == ApiCodes.SC_MINIMIZE)
+                {
+                    WindowStyle = WindowStyle.SingleBorderWindow;
+                    WindowState = WindowState.Minimized;
+                    handled = true;
+                }
+                else if (wParam.ToInt32() == ApiCodes.SC_RESTORE)
+                {
+                    WindowState = WindowState.Normal;
+                    WindowStyle = WindowStyle.None;
+                    handled = true;
+                }
+            }
+            return IntPtr.Zero;
+        }
         private void ShowSystemMenu(object sender, ExecutedRoutedEventArgs e)
         {
             var element = e.OriginalSource as FrameworkElement;
